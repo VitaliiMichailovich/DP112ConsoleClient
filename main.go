fbac49fa6350e42ea3036ff8cba15960ad202685
VitaliiMichailovich <vitaliy.michailovich@gmail.com>
2017-07-17 16:28:35 +0300

    wrong input

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"encoding/json"
 )
 
 func main() {
@@ -16,39 +17,37 @@ func main() {
 	var err error
 	taskId := flag.Int("task", 0, "an int")
 	fileJSON := flag.String("file", "", "a string")
+	link := flag.String("link", "http://localhost", "a string")
+	port := flag.Int("port", 8080, "an int")
+	url := *link+":"+strconv.Itoa(*port)
 	flag.Parse()
 	if *fileJSON == "" {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Please, write an address to json file with initial data.")
 		fileaddr, err = reader.ReadString('\n')
 		if err != nil {
-			fmt.Println(err.Error())
+			fmt.Println("Something went wrong \n", err.Error())
 			os.Exit(0)
 		}
 	} else {
 		fileaddr = *fileJSON
 	}
-
 	res, err := ioutil.ReadFile(fileaddr)
+	fmt.Println(fileaddr)
 	if err != nil {
-		return []byte{}, fmt.Errorf("Problem with json file opening with initial data. \n%v", err)
+		fmt.Println("Problem with json file opening with initial data. \n", err.Error())
 	}
-
-	if err != nil {
-		fmt.Println("Something went wrong.\n", err.Error())
-		os.Exit(0)
-	}
-
-	url := "http://localhost:8080"
 	var resource string
+	dataJson := res
 	if *taskId == 0 {
 		resource = url + "/tasks"
 	} else {
-
+		var params map[string]json.RawMessage
+		json.Unmarshal(res, &params)
 		resource = url + "/task/" + strconv.Itoa(*taskId)
+		dataJson = params[strconv.Itoa(*taskId)]
 	}
-	fmt.Println(resource, string(res))
-	req, err := http.NewRequest("POST", resource, bytes.NewBuffer(res))
+	req, err := http.NewRequest("POST", resource, bytes.NewBuffer(dataJson))
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
@@ -58,9 +57,27 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
+	//fmt.Println("response Status:", resp.Status)
+	//fmt.Println("response Headers:", resp.Header)
 	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
+	//fmt.Println("response Body:", string(body))
+	type WriteBack struct {
+		Task   int    `json:"task"`
+		Resp   string `json:"resp"`
+		Reason string `json:"reason"`
+	}
+	var returnData []WriteBack
 
+	err = json.Unmarshal(body, &returnData)
+	if err != nil {
+		var returnTask WriteBack
+		err = json.Unmarshal(body, &returnTask)
+		if err != nil {
+			fmt.Println("Blin")
+		}
+		fmt.Println("Task: ", returnTask.Task, "\nResult: ", returnTask.Resp, "\nReason: \n", returnTask.Reason)
+	}
+	for _, results := range returnData {
+		fmt.Println("\nTask: ", results.Task, "\nResult: ", results.Resp, "\nReason: ", results.Reason)
+	}
 }
